0fab097f93a68185556d13f56239e4e3745b02d6
OskolkovOleg <oskolkov.web@gmail.com>
2024-04-07 14:03:39 +0300

    Co-authored-by: lumik2006@gmail.com <lumik2006@gmail.com> Co-authored-by: AntichristSuperstar1312 <AntichristSuperstar1312@users.noreply.github.com> Co-authored-by: NOTAL1pser <NOTAL1pser@users.noreply.github.com> Co-authored-by: Fatummm <Fatummm@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import arcade
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE
 from views.IntroView import IntroView
 
-
 def main():
     window = arcade.Window(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, title=SCREEN_TITLE)
     menu_view = IntroView(window)
